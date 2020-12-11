d20e5db7ae7e62d8e244b11711cf19361a42183b
George Soderholm <georgesoderholm@gmail.com>
2020-12-11 16:07:04 -0700

    Add test for invoice

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Invoice do
-  xdescribe 'relationships' do
+  describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :merchant }
     it { should have_many :invoice_items }
