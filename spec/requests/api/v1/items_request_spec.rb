59a8041da21cbf91a7dfe3a8e39fc641fee53f2c
George Soderholm <georgesoderholm@gmail.com>
2020-12-12 12:46:10 -0700

    Add test to show presence of attributes of a single Item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -34,4 +34,36 @@ RSpec.describe 'Items API' do
       expect(item['attributes']['updated_at']).to be_a String
     end
   end
+
+  it 'can send a unique item information' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize: true)
+
+    expect(response).to be_successful
+    expect(item).to be_a Hash
+
+    expect(item['data']).to have_key('id')
+    expect(item['data']['id']).to be_a String
+
+    expect(item['data']).to have_key('type')
+    expect(item['data']['type']).to eq('item')
+
+    expect(item['data']['attributes']).to have_key('name')
+    expect(item['data']['attributes']['name']).to be_a String
+
+    expect(item['data']['attributes']).to have_key('description')
+    expect(item['data']['attributes']['description']).to be_a String
+
+    expect(item['data']['attributes']).to have_key('unit_price')
+    expect(item['data']['attributes']['unit_price']).to be_a Float
+
+    expect(item['data']['attributes']).to have_key('created_at')
+    expect(item['data']['attributes']['created_at']).to be_a String
+
+    expect(item['data']['attributes']).to have_key('updated_at')
+    expect(item['data']['attributes']['updated_at']).to be_a String
+  end
 end
