af8153ffe4aa908b6c8fd3dbe7943dbd02d276e0
George Soderholm <georgesoderholm@gmail.com>
2020-12-12 14:51:20 -0700

    Add #create method in Items to be able to create a new Item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,21 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    Item.reset_primary_key
+    item = Item.new(item_params)
+    return item.save ?
+      (render json:ItemSerializer.new(item)) : (render status: 404)
+  end
+
+  private
+
+  def self.reset_primary_key
+    ActiveRecord::Base.connection.reset_pk_sequence!('items')
+  end
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
+  end
 end
