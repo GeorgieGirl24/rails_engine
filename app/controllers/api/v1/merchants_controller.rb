684e17dc865af5cb6df0a2c8f73d8cd06c6b0e10
George Soderholm <georgesoderholm@gmail.com>
2020-12-12 14:20:08 -0700

    Add #create method to add a new Merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,21 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def create
+    Merchant.reset_primary_key
+    merchant = Merchant.new(merchant_params)
+    return merchant.save ?
+      (render json: MerchantSerializer.new(merchant)) : (render :status => 404)
+  end
+
+  private
+
+  def self.reset_primary_key
+    ActiveRecord::Base.connection.reset_pk_sequence!('merchants')
+  end
+
+  def merchant_params
+    params.permit(:name, :created_at, :updated_at)
+  end
 end
