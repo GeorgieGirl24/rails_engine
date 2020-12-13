049306d5283b2b64877ef85ecaefd01ce4cf146a
George Soderholm <georgesoderholm@gmail.com>
2020-12-12 23:45:06 -0700

    Add search_single to merchants model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   has_many :invoices, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.search_single(merchant_params)
+    where("#{merchant_params.to_h.keys.first} ILIKE ?",  "%#{merchant_params.to_h.values.first}%")
+  end
 end
