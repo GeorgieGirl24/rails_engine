a1725956fe0e6469880c0e6010158c3b5735c62d
George Soderholm <georgesoderholm@gmail.com>
2020-12-13 11:59:42 -0700

    Add tests that check it can return a merchant based on search parameters. Tests include partial word, all caps

--- spec/requests/api/v1/merchants/search_spec.rb
+++ spec/requests/api/v1/merchants/search_spec.rb
@@ -12,16 +12,16 @@ RSpec.describe 'Api::V1::Merchants::SearchController' do
     get "/api/v1/merchants/find?#{attribute}=#{search_term}"
     expect(response).to be_successful
 
-    json = JSON.parse(response.body, symbolic: true)['data']
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    # binding.pry
     merchant = json.last
     expect(merchant).to be_a Hash
-    expect(merchant['id'].to_i).to eq(merchant_2.id)
-    expect(merchant['id'].to_i).to_not eq(merchant_3.id)
-    expect(merchant['id'].to_i).to_not eq(merchant_1.id)
-    expect(merchant['attributes']['name']).to eq(merchant_2.name)
-    expect(merchant['attributes']['name']).to_not eq(merchant_1.name)
-    expect(merchant['attributes']['name'].downcase).to include(search_term.downcase)
-    # binding.pry
+    expect(merchant[:id].to_i).to eq(merchant_2.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_3.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_1.id)
+    expect(merchant[:attributes][:name]).to eq(merchant_2.name)
+    expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
+    expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
   end
 
   it 'can find a single merchant matching the search parameter with all caps' do
@@ -35,16 +35,64 @@ RSpec.describe 'Api::V1::Merchants::SearchController' do
     get "/api/v1/merchants/find?#{attribute}=#{search_term}"
     expect(response).to be_successful
 
-    json = JSON.parse(response.body, symbolic: true)['data']
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
     merchant = json.last
 
     expect(merchant).to be_a Hash
-    expect(merchant['id'].to_i).to eq(merchant_2.id)
-    expect(merchant['id'].to_i).to_not eq(merchant_3.id)
-    expect(merchant['id'].to_i).to_not eq(merchant_1.id)
-    expect(merchant['type']).to eq('merchant')
-    expect(merchant['attributes']['name']).to eq(merchant_2.name)
-    expect(merchant['attributes']['name']).to_not eq(merchant_1.name)
-    expect(merchant['attributes']['name'].downcase).to include(search_term.downcase)
+    expect(merchant[:id].to_i).to eq(merchant_2.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_3.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_1.id)
+    expect(merchant[:type]).to eq('merchant')
+    expect(merchant[:attributes][:name]).to eq(merchant_2.name)
+    expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
+    expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
+  end
+
+  it 'can find a single merchant matching the search parameter with part of the name' do
+    merchant_1 = create(:merchant, name: 'The Happily Ever Crafter')
+    merchant_2 = create(:merchant, name: 'The Pigeon Letters')
+    merchant_3 = create(:merchant, name: 'Her Name Is Mud')
+
+    attribute = :name
+    search_term = 'pig'
+
+    get "/api/v1/merchants/find?#{attribute}=#{search_term}"
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    merchant = json.last
+
+    expect(merchant).to be_a Hash
+    expect(merchant[:id].to_i).to eq(merchant_2.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_3.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_1.id)
+    expect(merchant[:type]).to eq('merchant')
+    expect(merchant[:attributes][:name]).to eq(merchant_2.name)
+    expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
+    expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
+  end
+  
+  it 'can find a single merchant matching the search parameter with only middle of name' do
+    merchant_1 = create(:merchant, name: 'The Happily Ever Crafter')
+    merchant_2 = create(:merchant, name: 'The Pigeon Letters')
+    merchant_3 = create(:merchant, name: 'Her Name Is Mud')
+
+    attribute = :name
+    search_term = 'igeo'
+
+    get "/api/v1/merchants/find?#{attribute}=#{search_term}"
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    merchant = json.last
+
+    expect(merchant).to be_a Hash
+    expect(merchant[:id].to_i).to eq(merchant_2.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_3.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_1.id)
+    expect(merchant[:type]).to eq('merchant')
+    expect(merchant[:attributes][:name]).to eq(merchant_2.name)
+    expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
+    expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
   end
 end
