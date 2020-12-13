1a65c72dbaf9e961407793aa9fa86c961cf9dbc4
George Soderholm <georgesoderholm@gmail.com>
2020-12-12 21:34:30 -0700

    Add dependent::destroy

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,7 +4,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
 
   has_many :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
   has_many :items, through: :invoice_items
 
 end
