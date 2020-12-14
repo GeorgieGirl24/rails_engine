0f6ab454bc80f88a29d8da419a6020e61d561fd0
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 16:47:26 -0700

    Add class methods in the Items file that find an Item with either the name or description, the unit_price or the created_at/updated_at parameters

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,22 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  def self.search_single(item_params)
+    search = item_params.values.first
+    attribute = item_params.keys.first
+    where("#{attribute} ILIKE ?",  "%#{search}%")
+  end
+
+  def self.search_date(item_params)
+    search = item_params.values.first
+    attribute = item_params.keys.first
+    where("DATE(#{attribute}) = ?", "%#{search}%")
+  end
+
+  def self.unit_price_search(item_params)
+    search = item_params.values.first
+    attribute = item_params.keys.first
+    where("to_char(#{attribute}, '99999999.99') ILIKE ?", "%#{search}%")
+  end
 end
