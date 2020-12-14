53e81f97fdf5e8bdd13edb7fbf443043c7b5dce3
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 15:51:54 -0700

    Refactor test to expect that the merchants name is returned when searching by date

--- spec/requests/api/v1/merchants/search_spec.rb
+++ spec/requests/api/v1/merchants/search_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe 'Api::V1::Merchants::SearchController' do
     expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
     expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
   end
-  
+
   it 'can find a single merchant matching the search parameter with only middle of name' do
     merchant_1 = create(:merchant, name: 'The Happily Ever Crafter')
     merchant_2 = create(:merchant, name: 'The Pigeon Letters')
@@ -94,5 +94,30 @@ RSpec.describe 'Api::V1::Merchants::SearchController' do
     expect(merchant[:attributes][:name]).to eq(merchant_2.name)
     expect(merchant[:attributes][:name]).to_not eq(merchant_1.name)
     expect(merchant[:attributes][:name].downcase).to include(search_term.downcase)
+  end
+
+  it 'can find a single merchant matching the search parameter with a created_at date' do
+    merchant_1 = create(:merchant,
+                        name: 'The Happily Ever Crafter',
+                        created_at: '2020-12-13')
+    merchant_2 = create(:merchant, name: 'The Pigeon Letters', created_at: '2020-10-13')
+    merchant_3 = create(:merchant, name: 'Her Name Is Mud', created_at: '2020-11-13')
+
+    attribute = :created_at
+    # search_term = '2020-11-13T06:13:28.643Z'
+    search_term = '2020-11-13'
+
+    get "/api/v1/merchants/find?#{attribute}=#{search_term}"
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    merchant = json.last
+# binding.pry
+    expect(merchant).to be_a Hash
+    expect(merchant[:id].to_i).to eq(merchant_3.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_2.id)
+    expect(merchant[:id].to_i).to_not eq(merchant_1.id)
+    expect(merchant[:type]).to eq('merchant')
+    expect(merchant[:attributes][:name]).to eq(merchant_3.name)
   end
 end
