25afdfdc71034bc98c16b13a8654e40440d4db51
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 15:52:37 -0700

    Remove pry

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.search_single(merchant_params))
+    if merchant_params[:name]
+      render json: MerchantSerializer.new(Merchant.search_single(merchant_params))
+    else merchant_params[:created_at] || merchant_params[:updated_at]
+      render json: MerchantSerializer.new(Merchant.search_date(merchant_params))
+    end
   end
 
   private
