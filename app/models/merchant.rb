a585eb9a9e3b39d59195089f25391a4f800ca047
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 15:51:19 -0700

    Refactor where statement to find date formatted correctly

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,18 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   def self.search_single(merchant_params)
-    where("#{merchant_params.to_h.keys.first} ILIKE ?",  "%#{merchant_params.to_h.values.first}%")
+    search = merchant_params.values.first
+    attribute = merchant_params.keys.first
+    where("#{attribute} ILIKE ?",  "%#{search}%")
+  end
+
+  def self.search_date(merchant_params)
+    search = merchant_params.values.first
+    attribute = merchant_params.keys.first
+    where("DATE(#{attribute}) = ?", "%#{search}%")
+    # wrap it in date look it up like doesn't like dates
+    # so use something else like '='
+    # where("to_char(#{attribute},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{search}%")
+    # binding.pry
   end
 end
