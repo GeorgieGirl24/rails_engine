508b1e619ba98fb005e2a03ea474e5e4aca7352f
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 20:02:09 -0700

    Add helper methods to help dry out code in the Item Model and add a #index method to call on the Item Model through the ItemSerializer

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -9,7 +9,19 @@ class Api::V1::Items::SearchController < ApplicationController
     end
   end
 
+  def index
+    render json: ItemSerializer.new(Item.search_multiple(attribute, search_term))
+  end
+
   private
+
+  def attribute
+    item_params.keys.first
+  end
+
+  def search_term
+    item_params[attribute]
+  end
 
   def item_params
     params.permit(:name, :description, :unit_price, :created_at, :updated_at)
