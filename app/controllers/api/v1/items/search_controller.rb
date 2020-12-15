e0a50912b087cc24c382b90f8dce21d24f155ef3
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 20:28:55 -0700

    Remove conditional logic from the controller and give it to the Item Model

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    if item_params[:name] || item_params[:description]
-      render json: ItemSerializer.new(Item.search_single(item_params))
-    elsif item_params[:created_at] || item_params[:updated_at]
-      render json: ItemSerializer.new(Item.search_date(item_params))
-    else
-      render json: ItemSerializer.new(Item.unit_price_search(item_params))
-    end
+    render json: ItemSerializer.new(Item.search_single(attribute, search_term))
   end
 
   def index
