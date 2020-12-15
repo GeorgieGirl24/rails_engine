1812180a126c118b87ffdc5d46093f11899f3411
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 18:30:41 -0700

    Add a #index method

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -7,7 +7,19 @@ class Api::V1::Merchants::SearchController < ApplicationController
     end
   end
 
+  def index
+    render json: MerchantSerializer.new(Merchant.search_multiple(attribute, search_term))
+  end
+  
   private
+
+  def attribute
+    merchant_params.keys.first
+  end
+
+  def search_term
+    merchant_params[attribute]
+  end
 
   def merchant_params
     params.permit(:name, :created_at, :updated_at)
