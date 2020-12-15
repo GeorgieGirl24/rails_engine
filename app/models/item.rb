23b7acef2517392cdeecf2d3c4286d8694e5d43c
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 20:01:07 -0700

    Add a .search_multiple method

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,15 +14,29 @@ class Item < ApplicationRecord
     where("#{attribute} ILIKE ?",  "%#{search}%")
   end
 
-  def self.search_date(item_params)
-    search = item_params.values.first
-    attribute = item_params.keys.first
+  def self.search_string(attribute, search)
+    where("#{attribute} ILIKE ?",  "%#{search}%")
+  end
+
+  def self.search_date(attribute, search)
+    # search = item_params.values.first
+    # attribute = item_params.keys.first
     where("DATE(#{attribute}) = ?", "%#{search}%")
   end
 
-  def self.unit_price_search(item_params)
-    search = item_params.values.first
-    attribute = item_params.keys.first
+  def self.unit_price_search(attribute, search)
+    # search = item_params.values.first
+    # attribute = item_params.keys.first
     where("to_char(#{attribute}, '99999999.99') ILIKE ?", "%#{search}%")
+  end
+
+  def self.search_multiple(attribute, search)
+    if attribute == 'created_at' || attribute == 'updated_at'
+      search_date(attribute, search)
+    elsif attribute == 'unit_price'
+      unit_price_search(attribute, search)
+    else
+      search_string(attribute, search)
+    end
   end
 end
