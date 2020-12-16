6ee207b15f0064fcfe103ee699bf045db54065bd
George Soderholm <georgesoderholm@gmail.com>
2020-12-15 20:32:58 -0700

    Add test to make sure the .most_revenue returned the correct number of merchants returned

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -64,5 +64,61 @@ describe Merchant do
       expect(response.first).to be_a Merchant
       expect(response.first[:name]).to be_a String
     end
+
+    it '.most_revenue' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+      merchant4 = create(:merchant)
+      merchant5 = create(:merchant)
+
+      invoice1 = create(:invoice, merchant_id: merchant1.id)
+      create(:transaction, result: 'success', invoice_id: invoice1.id)
+      create(:invoice_item, quantity: 1, unit_price: 10.00, invoice_id: invoice1.id, item_id: create(:item, unit_price: 10.00).id)
+
+      invoice2 = create(:invoice, merchant_id: merchant1.id)
+      create(:transaction, result: 'failed', invoice_id: invoice2.id)
+      create(:invoice_item, quantity: 1, unit_price: 10.00, invoice_id: invoice2.id, item_id: create(:item, unit_price: 10.00).id)
+
+      invoice3 = create(:invoice, merchant_id: merchant2.id)
+      create(:transaction, result: 'success', invoice_id: invoice3.id)
+      create(:invoice_item, quantity: 1, unit_price: 20.00, invoice_id: invoice3.id, item_id: create(:item, unit_price: 20.00).id)
+
+      invoice4 = create(:invoice, merchant_id: merchant2.id)
+      create(:transaction, result: 'failed', invoice_id: invoice4.id)
+      create(:invoice_item, quantity: 1, unit_price: 20.00, invoice_id: invoice4.id, item_id: create(:item, unit_price: 20.00).id)
+
+      invoice5 = create(:invoice, merchant_id: merchant3.id)
+      create(:transaction, result: 'success', invoice_id: invoice5.id)
+      create(:invoice_item, quantity: 1, unit_price: 30.00, invoice_id: invoice5.id, item_id: create(:item, unit_price: 30.00).id)
+
+      invoice6 = create(:invoice, merchant_id: merchant3.id)
+      create(:transaction, result: 'failed', invoice_id: invoice6.id)
+      create(:invoice_item, quantity: 1, unit_price: 30.00, invoice_id: invoice6.id, item_id: create(:item, unit_price: 30.00).id)
+
+      invoice7 = create(:invoice, merchant_id: merchant4.id)
+      create(:transaction, result: 'success', invoice_id: invoice7.id)
+      create(:invoice_item, quantity: 1, unit_price: 40.00, invoice_id: invoice7.id, item_id: create(:item, unit_price: 40.00).id)
+
+      invoice8 = create(:invoice, merchant_id: merchant4.id)
+      create(:transaction, result: 'failed', invoice_id: invoice8.id)
+      create(:invoice_item, quantity: 1, unit_price: 40.00, invoice_id: invoice8.id, item_id: create(:item, unit_price: 40.00).id)
+
+      invoice9 = create(:invoice, merchant_id: merchant5.id)
+      create(:transaction, result: 'success', invoice_id: invoice9.id)
+      create(:invoice_item, quantity: 1, unit_price: 50.00, invoice_id: invoice9.id, item_id: create(:item, unit_price: 50.00).id)
+
+      invoice10 = create(:invoice, merchant_id: merchant5.id)
+      create(:transaction, result: 'failed', invoice_id: invoice10.id)
+      create(:invoice_item, quantity: 1, unit_price: 50.00, invoice_id: invoice10.id, item_id: create(:item, unit_price: 50.00).id)
+
+      quantity = 3
+      response = Merchant.most_revenue(quantity)
+
+      expect(response.first).to be_a Merchant
+      expect(response.first[:name]).to eq(merchant5.name)
+      expect(response.second[:name]).to eq(merchant4.name)
+      expect(response.third[:name]).to eq(merchant3.name)
+    end
   end
 end
