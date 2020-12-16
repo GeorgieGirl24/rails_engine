37e358c047793b1a33b291fd07f2c3f3fb9b4a28
George Soderholm <georgesoderholm@gmail.com>
2020-12-16 15:33:47 -0700

    Update method to take an arguement of how many invoices you want returned

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,11 +9,11 @@ class Invoice < ApplicationRecord
 
   scope :successful, -> { where(status: 'shipped') }
 
-  def self.most_expensive
+  def self.most_expensive(quantity)
     joins(:invoice_items, :transactions)
       .merge(Transaction.successful)
       .select('invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
       .group(:id)
-      .order("revenue DESC").limit(5)
+      .order("revenue DESC").limit(quantity)
   end
 end
