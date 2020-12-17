0884ecad6b231dc3403a9df80e482b3cba329e42
George Soderholm <georgesoderholm@gmail.com>
2020-12-16 20:11:15 -0700

    Add static timestamps to the fixtures so that its easier to test

--- spec/requests/api/v1/merchants/single_merchant_revenue_spec.rb
+++ spec/requests/api/v1/merchants/single_merchant_revenue_spec.rb
@@ -4,27 +4,27 @@ RSpec.describe 'Single Merchant Revenue' do
   scenario 'can return the total revenue for a particular Merchant' do
     merchant1 = create(:merchant)
 
-    invoice1 = create(:invoice, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, merchant_id: merchant1.id, status: 'shipped', created_at: '2020-04-12T00:00:00 UTC')
     create(:transaction, result: 'success', invoice_id: invoice1.id)
     create(:invoice_item, quantity: 1, unit_price: 100.12, invoice_id: invoice1.id, item_id: create(:item, unit_price: 10.00).id)
 
-    invoice2 = create(:invoice, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, merchant_id: merchant1.id, status: 'packaged', created_at: '2020-06-01T00:00:00 UTC')
     create(:transaction, result: 'failed', invoice_id: invoice2.id)
     create(:invoice_item, quantity: 1, unit_price: 1_000.00, invoice_id: invoice2.id, item_id: create(:item, unit_price: 10.00).id)
 
-    invoice3 = create(:invoice, merchant_id: merchant1.id)
+    invoice3 = create(:invoice, merchant_id: merchant1.id, status: 'shipped', created_at: '2020-02-01T00:00:00 UTC')
     create(:transaction, result: 'success', invoice_id: invoice3.id)
     create(:invoice_item, quantity: 1, unit_price: 12_000.53, invoice_id: invoice3.id, item_id: create(:item, unit_price: 20.00).id)
 
-    invoice4 = create(:invoice, merchant_id: merchant1.id)
+    invoice4 = create(:invoice, merchant_id: merchant1.id, status: 'shipped', created_at: '2020-04-01T00:00:00 UTC')
     create(:transaction, result: 'success', invoice_id: invoice4.id)
     create(:invoice_item, quantity: 1, unit_price: 20.09, invoice_id: invoice4.id, item_id: create(:item, unit_price: 20.00).id)
 
-    invoice5 = create(:invoice, merchant_id: merchant1.id)
+    invoice5 = create(:invoice, merchant_id: merchant1.id, status: 'shipped', created_at: '2020-06-05T00:00:00 UTC')
     create(:transaction, result: 'success', invoice_id: invoice5.id)
     create(:invoice_item, quantity: 1, unit_price: 347.00, invoice_id: invoice5.id, item_id: create(:item, unit_price: 30.00).id)
 
-    invoice6 = create(:invoice, merchant_id: merchant1.id)
+    invoice6 = create(:invoice, merchant_id: merchant1.id, status: 'shipped', created_at: '2020-06-02T00:00:00 UTC')
     create(:transaction, result: 'failed', invoice_id: invoice6.id)
     create(:invoice_item, quantity: 1, unit_price: 30.00, invoice_id: invoice6.id, item_id: create(:item, unit_price: 30.00).id)
 
