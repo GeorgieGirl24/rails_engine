5faf4ae977913f11488baac0d50c5003c36e484b
George Soderholm <georgesoderholm@gmail.com>
2020-12-17 22:17:09 -0700

    Add Api::V1::Invoices::StatisticsController

--- app/controllers/api/v1/invoices/statistics_controller.rb
+++ app/controllers/api/v1/invoices/statistics_controller.rb
@@ -2,6 +2,5 @@ class Api::V1::Invoices::StatisticsController < ApplicationController
   def most_expensive
     quantity = params[:quantity]
     render json: InvoiceSerializer.new(Invoice.most_expensive(quantity))
-    # binding.pry
   end
 end
